08015db6f38e00ead3aaa6e19c466a1725b14c7c
Nikela Papadopoulou <nikela.papadopoulou@glasgow.ac.uk>
2024-11-04 19:07:04 +0000

    Add carbon footprint estimation and forecast

--- src/plugins/job_submit/carbon_logging/job_submit_carbon_logging.c
+++ src/plugins/job_submit/carbon_logging/job_submit_carbon_logging.c
@@ -42,6 +42,18 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <regex.h>
+#include <curl/curl.h>
+#include <time.h>
+
+#include "config.h"
+
+#if HAVE_JSON_C_INC
+#include <json-c/json.h>
+#else
+#include <json/json.h>
+#endif
+
 
 #include "slurm/slurm_errno.h"
 #include "src/common/slurm_xlator.h"
@@ -49,6 +61,44 @@
 #include "src/common/xstring.h"
 #include "src/slurmctld/locks.h"
 #include "src/slurmctld/slurmctld.h"
+
+#define KHW 2.77778e-7
+#ifndef LCA
+#define LCA 5*365*24*60 //5 years in mins
+#endif
+
+#ifndef POWER_CPU
+#define POWER_CPU 165 //W
+#endif
+
+#ifndef POWER_GPU
+#define POWER_GPU 300 //W
+#endif
+
+#ifndef POWER_MEM
+#define POWER_MEM 1 // W per 8GB
+#endif
+
+#ifndef EM_CPU
+#define EM_CPU 10 //Intel Xeon Gold 6240R (14nm, 24 cores) kgCO2
+#endif
+
+#ifndef EM_GPU
+#define EM_GPU 20 // NVIDIA V100 kgCO2
+#endif
+
+#ifndef EM_MEM
+#define EM_MEM 1 // 8GB module
+#endif
+
+/* Required to get the carbon intensity */
+
+#define TIME_FORMAT "%Y-%m-%dT%H:%M:%SZ"
+#define MAX_RECORDS 48 // 24h, 30min per record. It should be no more than 48
+#ifndef REGION_ID
+#define REGION_ID 16 // Scotland
+#endif
+#define MAX_SCI_VALUE 10000 //
 
 /*
  * These variables are required by the generic plugin interface.  If they
@@ -84,6 +134,263 @@ const uint32_t plugin_version   = SLURM_VERSION_NUMBER;
  * plugin. If you develop another plugin that may be of interest to others
  * please post it to slurm-dev@schedmd.com  Thanks!
 \*****************************************************************************/
+
+
+
+/* Parses tres_per_node and returns the requested GPUs per node */
+extern int regex_gpus(char *tres_per_node)
+{
+	const char *pattern = "gres:gpu:([0-9]+)";
+	regex_t regex;
+	regmatch_t pmatch[2];
+	char match[10];
+	int num_gpus = 0;
+
+	if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+		error("Could not compile regex");
+		return 0;
+	}
+
+	if (regexec(&regex, tres_per_node, 2, pmatch, 0) == 0) {
+		// Extract the matched substring for GPU count
+		int len = pmatch[1].rm_eo - pmatch[1].rm_so;
+		strncpy(match, tres_per_node + pmatch[1].rm_so, len);
+		match[len] = '\0';
+		num_gpus = atoi(match);
+	} else {
+		error("No match found");
+	}
+
+	regfree(&regex);
+
+	return num_gpus;
+}
+
+/* Returns current time in ISO8601 format in from */
+void get_iso8601_time(char *from, size_t buffer_size) {
+	time_t now = time(NULL);
+	struct tm *tm_info = gmtime(&now);
+	strftime(from, buffer_size, TIME_FORMAT, tm_info);
+}
+
+/* Callback to handle the HTTP response */
+static size_t _write_callback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    char **buffer = (char**)userp;
+	if (*buffer == NULL) {
+		*buffer = (char*)malloc(1);
+	}
+
+    // Allocate a new buffer with the desired size
+    char *new_buffer = malloc(strlen(*buffer) + realsize + 1);
+    if (new_buffer == NULL) {
+        fprintf(stderr, "out of memory\n");
+        return 0;
+    }
+
+    // Copy the old buffer contents to the new buffer
+    strcpy(new_buffer, *buffer);
+
+    // Copy the new data to the end of the new buffer
+    memcpy(&new_buffer[strlen(*buffer)], contents, realsize);
+    new_buffer[strlen(*buffer) + realsize] = '\0';
+
+    // Free the old buffer
+	if (**buffer)
+	    free(*buffer);
+
+    // Update the buffer pointer
+    *buffer = new_buffer;
+
+    return realsize;
+}
+
+uint32_t _get_intensity(char *url) {
+ CURL *curl;
+    CURLcode res;
+	char * response="";
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, _write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+	}
+	 json_object *root_obj = json_tokener_parse(response);
+    if (!root_obj) {
+        fprintf(stderr, "Error parsing JSON\n");
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+		return -1;
+
+    }
+	json_object *first_region;
+    json_object *data_array = json_object_object_get(root_obj, "data");
+    if (!data_array || !json_object_is_type(data_array, json_type_array)) {
+        //fprintf(stderr, "Error: 'data' is not an array\n");
+		if (!data_array) {
+        	json_object_put(root_obj);
+        	curl_easy_cleanup(curl);
+        	free(response);
+			return -1;
+		}
+		first_region = data_array;
+    }
+	else {
+
+    first_region = json_object_array_get_idx(data_array, 0);
+    if (!first_region || !json_object_is_type(first_region, json_type_object)) {
+        fprintf(stderr, "Error: First region is not an object\n");
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+		return -1;
+
+            }
+	}
+    json_object *region_data_array = json_object_object_get(first_region, "data");
+    if (!region_data_array || !json_object_is_type(region_data_array, json_type_array)) {
+        fprintf(stderr, "Error: 'data' in region is not an array\n");
+//        json_object_put(root_obj);
+//        curl_easy_cleanup(curl);
+//        free(response);
+//		return -1;
+
+    }
+
+    json_object *first_data_item = json_object_array_get_idx(region_data_array, 0);
+    if (!first_data_item || !json_object_is_type(first_data_item, json_type_object)) {
+        fprintf(stderr, "Error: First data item is not an object\n");
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+		return -1;
+
+    }
+
+    json_object *intensity_obj = json_object_object_get(first_data_item, "intensity");
+    if (!intensity_obj || !json_object_is_type(intensity_obj, json_type_object)) {
+        fprintf(stderr, "Error: 'intensity' is not an object\n");
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+		return -1;
+
+    }
+
+    json_object *forecast_value = json_object_object_get(intensity_obj, "forecast");
+    if (!forecast_value || !json_object_is_type(forecast_value, json_type_int)) {
+        fprintf(stderr, "Error: 'forecast' is not an integer\n");
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+		return -1;
+    }
+
+    uint32_t forecast = json_object_get_int(forecast_value);
+
+	cleanup:
+
+        json_object_put(root_obj);
+        curl_easy_cleanup(curl);
+        free(response);
+	return forecast;		
+}
+
+
+extern uint32_t _get_intensity_now() {
+	// https://carbon-intensity.github.io/api-definitions/#get-regional-regionid-regionid
+	char from[25];
+	char url[256];
+	size_t record_count;
+
+	get_iso8601_time(from, sizeof(from));
+	snprintf(url, sizeof(url),
+	 		"https://api.carbonintensity.org.uk/regional/regionid/%d",
+	 		 REGION_ID);
+	return get_intensity(url);
+}
+
+extern uint32_t _get_intensity_forecast() {
+	// doc: https://carbon-intensity.github.io/api-definitions/#get-regional-intensity-from-fw24h
+	char from[25];
+	char url[256];
+	size_t record_count;
+
+	get_iso8601_time(from, sizeof(from));
+	snprintf(url, sizeof(url),
+	 		"https://api.carbonintensity.org.uk/regional/intensity/%s/fw24h/regionid/%d",
+	 		from, REGION_ID);
+	return get_intensity (url);
+}
+
+
+extern void _compute_carbon(job_desc_msg_t *job_desc, part_record_t * part_ptr, uint32_t carbon_intensity) 
+{
+	
+
+
+	uint32_t current_intensity = _get_intensity_now();
+	uint32_t future_intensity = _get_intensity_forecast();
+	
+
+	// SCI = (operational carbon + embodied carbon) / walltime
+	uint32_t walltime =job_desc->time_limit; // mins
+
+	// Energy = Number of Nodes * Power of the node * Walltime
+	uint32_t num_nodes = job_desc->num_tasks / job_desc->ntasks_per_node;
+
+	uint16_t cpus_per_task = job_desc->cpus_per_task == (uint16_t)-1? 1: job_desc->cpus_per_task;
+	uint32_t total_cpus = part_ptr->max_core_cnt; // could also be max_cpu_cnt;
+	uint32_t total_memory = part_ptr->max_mem_per_cpu;
+	// Power of the node = Power of the CPU + Power of the GPUs + Power of DRAM + Power of any SSDs/HDDs
+	uint32_t power_cpus = ((cpus_per_task * job_desc->ntasks_per_node) / total_cpus) * POWER_CPU; // W
+	uint32_t power_mem = ((job_desc->pn_min_memory) ? job_desc->pn_min_memory : part_ptr->max_mem_per_cpu)*POWER_MEM/8.;
+	uint32_t power_gpus = 0;
+	int gpus_per_node = 0;
+	if (job_desc->tres_per_node != NULL) {
+		gpus_per_node = regex_gpus(job_desc->tres_per_node);
+		power_gpus = POWER_GPU * gpus_per_node;
+	}
+
+	uint32_t power_nodes = power_cpus + power_gpus + power_mem;
+
+	uint32_t energy = num_nodes * power_nodes * walltime * 60; // J = W * sec
+
+	// Opertional Emissions = Energy * Region-specific carbon intensity
+	double op_emissions = ((double)energy * KWH) * current_intensity; // kWH * gCO2/kWh -> gCO2
+	double op_emissions_forecast = ((double)energy * KWH) *future_intensity;
+
+	// Embodied Emissions = Total embodied emissions * Time share * Resource share
+	double time_share = (double)walltime / LCA; // mins/mins
+	double em_cpu = ((cpus_per_task * job_desc->ntasks_per_node) / total_cpus)*EM_CPU ;
+	double em_gpu = job_desc->tres_per_node != NULL ? job_desc->tres_per_node * EM_GPU : 0;
+	double em_mem = ((job_desc->pn_min_memory) ? job_desc->pn_min_memory : part_ptr->max_mem_per_cpu)*EM_MEM/8.;
+	double em_emissions = (em_cpu + em_gpu + em_mem) * num_nodes * time_share; // gCO2
+
+	job_desc->power_est = num_nodes * power_nodes;
+	job_desc->energy_est = energy;
+	job_desc->sci_est = op_emissions + em_emissions;
+	job_desc->sci_fcst = op_emissions_forecast + em_emissions;
+	double change_rate = (future_intensity - current_intensity) / current_intensity;
+
+	if (change_rate > 0) {
+		job_desc->change_rate = change_rate + current_sci / MAX_SCI_VALUE;
+	} else {
+		job_desc->change_rate = change_rate - current_sci / MAX_SCI_VALUE;
+	}
+
+
+
+}
+
 
 /* Test if this user can run jobs in the selected partition based upon
  * the partition's AllowGroups parameter. */
@@ -135,6 +442,20 @@ static bool _valid_memory(part_record_t *part_ptr, job_desc_msg_t *job_desc)
 		/* Perform per node memory limit test */
 		job_limit  = job_desc->pn_min_memory;
 		part_limit = part_ptr->max_mem_per_cpu;
+	} else {
+		/* Can not compare per node to per CPU memory limits */
+		return true;
+	}
+
+	if (job_limit > part_limit) {
+		debug("job_submit/partition: skipping partition %s due to "
+		      "memory limit (%"PRIu64" > %"PRIu64")",
+		      part_ptr->name, job_limit, part_limit);
+		return false;
+	}
+
+	return true;
+}
 
 /* Get maximum resources supported on partition nodes - 
 	required to estimate power share */
@@ -191,11 +512,14 @@ extern int job_submit(job_desc_msg_t *job_desc, uint32_t submit_uid,
 	 * Note that default values for most numbers is NO_VAL */
 	info("Job submit request: account:%s begin_time:%ld dependency:%s "
 	     "name:%s partition:%s qos:%s submit_uid:%u time_limit:%u "
-	     "user_id:%u sci:%.3f",
+	     "user_id:%u power_est:%.3f energy_est:%.3f sci_est:%.3f"
+		 "sci_fcst:%.3f change_rate:%.3f",
 	     job_desc->account, (long)job_desc->begin_time,
 	     job_desc->dependency,
 	     job_desc->name, job_desc->partition, job_desc->qos,
-	     submit_uid, job_desc->time_limit, job_desc->user_id, job_desc->est_sci);
+	     submit_uid, job_desc->time_limit, job_desc->user_id, 
+		 job_desc->power_est, job_desc->energy_est, job_desc->sci_est,
+		 job_desc->sci_fcst, job_desc->change_rate);
 
 
 	return SLURM_SUCCESS;
